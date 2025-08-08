cce2b5328767d45b26bf0bbcf16f48c589ba01e8
axxelng <angel950240@gmail.com>
2025-08-08 16:54:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 初始化 Supabase
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const SUPABASE_URL = "https://eucslvrdocoxrodttipy.supabase.co";
+const SUPABASE_URL = "https://awszlgououthest-1.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1Y3NsdnJkb2NveHJvZHR0aXB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ2MzU3NDAsImV4cCI6MjA3MDIxMTc0MH0.hPPmz92thDkeO-tr58raZrngJrnAdW_iIS79KmeVxOY";
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
